218f7ab829c34f8264d616789b16189940f93004
Daffa Afifi Syahrony <afifidaffa16@gmail.com>
2022-09-28 08:15:25 +0700

    modified acara12/latihan2.php

--- acara12/latihan2.php
+++ acara12/latihan2.php
@@ -46,12 +46,32 @@ class Tabung extends Kriteria implements Kolam{
     }
 }
 
+class Kubus extends Kriteria implements Kolam{
+    private $rusuk;
+
+    public function __construct($rusuk){
+        $this->rusuk = $rusuk;
+    }
+
+    public function hitungVolumeKolam(){
+        return $this->rusuk * $this->rusuk * $this->rusuk;
+    }
+
+    public function tinggiMinimal(){
+        return "tinggi minimal 80 cm";
+    }
+}
+
 $kolamDewasa = new Balok(10, 5, 1.5);
 $kolamAnak = new Tabung(5, 1);
+$kolamBalita = new Kubus(0.5);
 
 echo "Kolam Dewasa memiliki volume air sebesar " . $kolamDewasa->hitungVolumeKolam() . " m3 ";
 echo "Dengan persyaratan " . $kolamDewasa->tinggiMinimal() . "<br>";
 
 echo "Kolam Anak memiliki volume air sebesar " . $kolamAnak->hitungVolumeKolam() . " m3 ";
-echo "Dengan persyaratan " . $kolamAnak->tinggiMinimal();
+echo "Dengan persyaratan " . $kolamAnak->tinggiMinimal() . "<br>";
+
+echo "Kolam Balita memiliki volume air sebesar " . $kolamBalita->hitungVolumeKolam() . " m3 ";
+echo "Dengan persyaratan " . $kolamBalita->tinggiMinimal();
 ?>
