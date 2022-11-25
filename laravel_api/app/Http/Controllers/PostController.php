4641ed1e5c814d83afe835316320e8486283275a
DaffaAfifi <afifidaffa16@gmail.com>
2022-11-26 00:19:56 +0700

    api resource

--- laravel_api/app/Http/Controllers/PostController.php
+++ laravel_api/app/Http/Controllers/PostController.php
@@ -4,11 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Resources\PostResource;
+use App\Http\Resources\PostDetailResource;
 
 class PostController extends Controller
 {
     public function index(){
         $posts = Post::all();
-        return response()->json($posts);
+        // api biasa
+        // return response()->json(['data' => $posts]);
+
+        // api resource
+        return PostResource::collection($posts);
+    }
+
+    public function show($id){
+        $post = Post::with('writer:id,username')->findOrFail($id);
+
+        // api biasa
+        // return response()->json(['data' => $post]);
+
+        // api resource
+        return new PostDetailResource($post);
     }
 }
