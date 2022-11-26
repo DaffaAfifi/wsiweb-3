2c8a828073291abf5f727a6e22591bdb3ca87d8a
DaffaAfifi <afifidaffa16@gmail.com>
2022-11-26 14:51:14 +0700

    CRUD API laravel

--- laravel_api/app/Http/Controllers/PostController.php
+++ laravel_api/app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Http\Resources\PostResource;
 use App\Http\Resources\PostDetailResource;
+use Auth;
 
 class PostController extends Controller
 {
@@ -15,7 +16,7 @@ class PostController extends Controller
         // return response()->json(['data' => $posts]);
 
         // api resource
-        return PostResource::collection($posts);
+        return PostDetailResource::collection($posts->loadMissing('writer:id,username'));
     }
 
     public function show($id){
@@ -26,5 +27,34 @@ class PostController extends Controller
 
         // api resource
         return new PostDetailResource($post);
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'title' => 'required|max:255',
+            'news_content' => 'required'
+        ]);
+
+        $request['author'] = Auth::user()->id;
+        $post = Post::create($request->all());
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
+    }
+
+    public function update(Request $request, $id){
+        $request->validate([
+            'title' => 'required|max:255',
+            'news_content' => 'required'
+        ]);
+
+        $post = Post::findOrFail($id);
+        $post->update($request->all());
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
+    }
+
+    public function destroy($id){
+        $post = Post::findOrfail($id);
+        $post->delete();
+
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
     }
 }
